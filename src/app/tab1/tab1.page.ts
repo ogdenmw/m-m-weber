da977344f604cda07430f92540d7b70fe143db5d
mikeogdench <84337953+mikeogdench@users.noreply.github.com>
2023-09-04 11:33:15 +0200

    parallax seems to be working pretty well

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -16,33 +16,55 @@ export class Tab1Page {
   topImage: any;
   topMiddleImage: any;
   topBottomImage: any;
-  
+
   headerHeight: number;
-  moveImage: number;
-  scaleImage: number; 
+  moveImageTop: number;
+  scaleImageTop: number; 
+
+  moveImageTopMiddle: number;
+  scaleImageTopMiddle: number; 
+
+  moveImageTopBottom: number;
+  scaleImageTopBottom: number; 
 
   constructor(public element: ElementRef, public renderer: Renderer2, private domCtrl: DomController) {}
 
   handleScroll(ev: CustomEvent<ScrollDetail>) {
 
-        console.log('scroll', ev.detail);
+        //console.log('scroll', ev.detail);
         // this.scrollElem = this.content.getScrollElement();
         // this.header = document.getElementsByClassName('parallax-image')[0];
     
         const scrollTop=ev.detail.scrollTop; 
-        console.log('scrollTop: ', scrollTop);
+        //console.log('scrollTop: ', scrollTop);
   
         this.domCtrl.write(() => {
           if (scrollTop > 0) {
-            this.moveImage = scrollTop / 2;
-            this.scaleImage = 1;
+            this.moveImageTop = scrollTop / 3;
+            this.scaleImageTop = 1;
+
+            this.moveImageTopMiddle = scrollTop / 2;
+            this.scaleImageTopMiddle = 1;            
+
+            this.moveImageTopBottom = scrollTop / 5;
+            this.scaleImageTopBottom = 1;            
+
           } else {
   
           }
   
           this.renderer.setStyle(this.topImage, 
-            'webkitTransform', 'translate3d(0, ' + this.moveImage + 'px,0) scale(' + this.scaleImage + ')'
+            'webkitTransform', 'translate3d(0, ' + this.moveImageTop + 'px,0) scale(' + this.scaleImageTop + ')'
             )
+
+          this.renderer.setStyle(this.topMiddleImage, 
+            'webkitTransform', 'translate3d(0, ' + this.moveImageTopMiddle + 'px,0) scale(' + this.scaleImageTopMiddle + ')'
+            )
+
+          this.renderer.setStyle(this.topBottomImage, 
+            'webkitTransform', 'translate3d(0, ' + this.moveImageTopBottom + 'px,0) scale(' + this.scaleImageTopBottom + ')'
+            )
+    
           });
 
   }
@@ -50,10 +72,12 @@ export class Tab1Page {
   ngOnInit() {
     let content = this.element.nativeElement;
     this.topImage = document.getElementById('topImage');    
+    this.topMiddleImage = document.getElementById('topMiddleImage');    
+    this.topBottomImage = document.getElementById('topBottomImage');    
 
     this.domCtrl.read(() => {
       this.headerHeight = this.topImage.clientHeight; 
-      console.log('headerHeight: ', this.headerHeight);
+      //console.log('headerHeight: ', this.headerHeight);
   });
 
 
