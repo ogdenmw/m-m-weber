65c92beb9d61ca15a831f0057bd30e3cf2fb9e65
mikeogdench <84337953+mikeogdench@users.noreply.github.com>
2023-09-04 14:58:27 +0200

    improvements to the footer. more needed

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ScreensizeService } from '../services/screensize.service';
+import { LoadingController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tabs',
@@ -10,11 +12,22 @@ export class TabsPage {
   isDesktop: boolean = false;
   mmweberlogo: string ='assets/mmweber-logo.svg';
 
-  constructor(private screensizeService: ScreensizeService) {
+  constructor(private screensizeService: ScreensizeService, public loadingController: LoadingController) {
     this.screensizeService.isDesktopView().subscribe(isDesktop => {
       console.log('is desktop changed: ', isDesktop);
       this.isDesktop = isDesktop;
-    })
+    })    
   }
 
+  ngOnInit() {
+    // this is how to show a loading spinner, in case we ever want to implement this. 
+    // when we implement it, we probably want to do it as a service so we can use it on any page
+    // this.loadingController.create({
+    //   message: 'This Loader Will Auto Hide in 2 Seconds',
+    //   duration: 2000,
+    //   cssClass: 'loaderClass'
+    // }).then((res) => {
+    //   res.present();
+    // });
+  };
 }
