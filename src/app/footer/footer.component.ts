8495721b9eac9607fbcb7d9e26487371ea8122f2
mikeogdench <84337953+mikeogdench@users.noreply.github.com>
2023-12-03 21:02:42 +0100

    simplification of button classes. added footer component

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { IonContent } from '@ionic/angular';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
